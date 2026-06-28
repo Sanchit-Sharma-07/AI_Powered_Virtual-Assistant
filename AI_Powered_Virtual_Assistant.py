d805c5ae010fa0047f1eb26c8c507263bc60aa7b
Sanchit Sharma <sharmasanchit432@gmail.com>
2026-06-28 17:00:10 +0530

    Initial Commit

---
+++ AI_Powered_Virtual_Assistant.py
@@ -0,0 +1,20 @@
+from google import genai
+
+client = genai.Client(api_key="AIzaSyCUJWrifxYPLDdhstZw_Pp5MpaHare7xnE")
+
+def ask_ai(query):
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",  
+        contents=query
+    )
+    return response.text
+
+print("🤖AI Assistant Ready! (type 'exit' to quit)\n")
+
+while True:
+    user_input = input("You: ")
+    if user_input.lower() in ["exit", "quit", "bye"]:
+        print("AI: Goodbye! 👋")
+        break
+    reply = ask_ai(user_input)
+    print(f"AI: {reply}")
